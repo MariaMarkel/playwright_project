b48e2c92e97e457ffb69c9fdce454e4ac267e11c
MariaMarkel <volkova0512@gmail.com>
2022-03-06 15:58:10 -0500

    add beforeAll in amazonItem.spec.js, add tracing in sandbox.spec.js

--- tests/amazonItem.spec.js
+++ tests/amazonItem.spec.js
@@ -1,30 +1,37 @@
 // @ts-check
 //const { chromium } = require('playwright');
-const { test, chromium, expect } = require('@playwright/test');
+const { test, chromium, expect, Page } = require('@playwright/test');
 const  { AmazonMainPage }  = require('./../pageObjects/amazonMainPage');
 const { ProductPage } = require("../pageObjects/productPage");
 
-test.beforeEach(async ({ page }) => { //а ты уверена что хочешь не beforeAll?
-     // const browser = await chromium.launch({ headless: false });
-     // const context = await browser.newContext({
-     //     recordVideo: {dir: `videos`}
-     // });
-    //const page = await context.newPage();
+// test.beforeEach(async ({ page }) => { //а ты уверена что хочешь не beforeAll?
+//      // const browser = await chromium.launch({ headless: false });
+//      // const context = await browser.newContext({
+//      //     recordVideo: {dir: `videos`}
+//      // });
+//     //const page = await context.newPage();
 
+//     const amazonMainPage = new AmazonMainPage(page);
+//     await amazonMainPage.goto(); //можно вынести в переменную URL
+//     console.log(page.url);
+// });
+let page;
+test.beforeAll(async ({browser}) => {
+    page = await browser.newPage();
     const amazonMainPage = new AmazonMainPage(page);
-    await amazonMainPage.goto(); //можно вынести в переменную URL
-    console.log(page.url);
-});
+    await amazonMainPage.goto();
+    console.log(page.url)
+})
 
-test('0: go to amazon.com', async ({page}) => {
+test('0: go to amazon.com', async () => {
     expect(page.url()).toBe('https://www.amazon.com/');
 });
 
-test('1: go to amazon.com', async ({page}) => {
+test('1: go to amazon.com', async () => {
     await expect(page).toHaveURL('https://www.amazon.com/');
 });
 
-test('2: search for roswear jeans', async ({page}) => {
+test('2: search for roswear jeans', async () => {
     const amazonMainPage = new AmazonMainPage(page);
     //const productPage = new ProductPage(page);
     const input = await page.isVisible('input#twotabsearchtextbox');
@@ -36,7 +43,7 @@ test('2: search for roswear jeans', async ({page}) => {
     // await expect(productPage.productTitle).toHaveText('        roswear Women\'s Essentials Ripped Mid Rise Destroyed Skinny Jeans       ');
 });
 
-test('3: verify the name of selected item', async ({page}) => {
+test('3: verify the name of selected item', async () => {
     const amazonMainPage = new AmazonMainPage(page);
     const productPage = new ProductPage(page);
     amazonMainPage.firstItemInList.click();
@@ -44,13 +51,16 @@ test('3: verify the name of selected item', async ({page}) => {
     await expect(productPage.productTitle).toHaveText('        roswear Women\'s Essentials Ripped Mid Rise Destroyed Skinny Jeans       ');
 });
 
-test('4: verify the price of selected item', async ({ page }) => {
+test('4: verify the price of selected item', async () => {
     const productPage = new ProductPage(page);
     await expect(productPage.productPrice).toHaveText('$38.99');
     console.log(productPage.productPrice);
 });
-test.afterEach(async () => {
-    // await context.close(); //visible only within the hook
-    // await browser.close();
-    console.log("we finish")
-})
+// test.afterEach(async () => {
+//     // await context.close(); //visible only within the hook
+//     // await browser.close();
+//     console.log("we finish")
+// })
+test.afterAll(async () => {
+    await page.close();
+  });
--- tests/sandbox.spec.js
+++ tests/sandbox.spec.js
@@ -2,14 +2,16 @@ const { test, expect } = require("@playwright/test");
 const { AmazonMainPage } = require("../pageObjects/amazonMainPage");
 const { ProductPage } = require("../pageObjects/productPage");
 
-test('testing e2e flow', async ({page}) => {
+test('testing e2e flow', async ({ page, context }) => {
     const amazonMainPage = new AmazonMainPage(page);
     const productPage = new ProductPage(page);
+    await context.tracing.start({ screenshots: true, snapshots: true });
     await amazonMainPage.goto();
     amazonMainPage.searchField.fill('roswear women jeans');
     amazonMainPage.submitButton.click();
-    await page.screenshot({path: `list.png`});
+    await page.screenshot({ path: `list.png` });
     amazonMainPage.firstItemInList.click();
-    await expect(productPage.productTitle).toHaveText('        roswear Women\'s Essentials Ripped Mid Rise Destroyed Skinny Jeans       ');
+    await expect(productPage.productTitle).toHaveText('roswear Women\'s Essentials Ripped Mid Rise Destroyed Skinny Jeans');
     await expect(productPage.productPrice).toHaveText('$38.99');
+    await context.tracing.stop({ path: 'trace.zip' });
 });
