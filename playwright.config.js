f87544dac17b5d58a7d605d9e51e67b569b4acf8
MariaMarkel <volkova0512@gmail.com>
2022-03-31 22:56:12 -0400

    update package.json, purchaseWorkflow.spec.js, work in process

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,6 @@
 // @ts-check
 const { devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- * @type {import('@playwright/test').PlaywrightTestConfig}
- */
 const config = {
   testDir: './tests',
   /* Maximum time one test can run for. */
@@ -19,7 +8,7 @@ const config = {
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
+     * For example in await expect(locator).toHaveText();
      */
     timeout: 5000
   },
@@ -30,16 +19,19 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    /* Maximum time each action such as click() can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    /* Base URL to use in actions like await page.goto('/'). */
+    baseURL: 'https://amazon.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    /* Coolect video for test */
+    video: 'on',
+    headless: false
   },
 
   /* Configure projects for major browsers */
--- tests/purchaseWorkflow.spec.js
+++ tests/purchaseWorkflow.spec.js
@@ -1,52 +1,56 @@
 // @ts-check
-const { test, chromium, expect, Page } = require('@playwright/test');
-const  { AmazonMainPage }  = require('./../pageObjects/amazonMainPage');
+const { test, expect } = require('@playwright/test');
+const { AmazonMainPage } = require('./../pageObjects/amazonMainPage');
 const { ProductPage } = require("../pageObjects/productPage");
+// const {computer} = require('./../utils/consts')
 
-let page;
 let context;
 test.beforeAll(async ({browser}) => {
-    page = await browser.newPage();
-    context = await browser.newContext({
-        recordVideo: {dir: `videos`}
-    });
-    const amazonMainPage = new AmazonMainPage(page);
-    await context.tracing.start({ screenshots: true, snapshots: true });
-    await amazonMainPage.goto();
-})
-
-test('1: go to amazon.com', async () => {
-    expect(page.url()).toBe('https://www.amazon.com/');
+    context = await browser.newContext({ recordVideo: { dir: 'videos/firstVideo' } });
 });
 
-test('2: go to all computer accessories', async () => {
+test.beforeEach(async ({ page }) => {
+    const amazonMainPage = new AmazonMainPage(page);
+    await amazonMainPage.goto();
+});
+
+test('1: go to amazon.com', async ({ page }) => {
+    //page.video()
+    expect(page.url()).toBe('https://www.amazon.com/');
+    //expect(await page.screenshot()).toMatchSnapshot('amazon-maim.png');
+});
+
+
+test('2: go to all computer accessories', async ({ page }) => {
     const amazonMainPage = new AmazonMainPage(page);
     await amazonMainPage.allDropdown.click();
     await amazonMainPage.seeAllDepartmentsDropdown.first().click();
     await amazonMainPage.computersDropdown.nth(0).click();
-    await page.screenshot({ path: `seeComputers.png` });
+    await page.screenshot({ path: `screen1.png` });
     await amazonMainPage.computersCategories.nth(4).click();
-    expect(amazonMainPage.dropdownBoxCategory).toHaveText('Computers');
+    expect(amazonMainPage.dropdownBoxCategory).toHaveText("Computer");
 });
 
-test('3: select Apple computer mouse', async () => {
+test('3: select Apple computer mouse', async ({ page }) => {
     const amazonMainPage = new AmazonMainPage(page);
     const productPage = new ProductPage(page);
     amazonMainPage.appleCheckbox.nth(2).click();
     //await page.screenshot({ path: `selectAppleCheck.png` });
     productPage.appleMagicMouse.click();
-    await page.screenshot({ path: `seeMouse.png` });
+    await page.screenshot({ path: `scr2.png` });
     expect(productPage.addToCartButton).toBeVisible;
 });
 
-test.fixme('4: add Apple computer mouse to cart', async () => {
+
+
+test.fixme('4: add Apple computer mouse to cart', async ({ page }) => {
+    const amazonMainPage = new AmazonMainPage(page);
     const productPage = new ProductPage(page);
     productPage.addToCart();
-    //await page.screenshot({ path: `addedToCart.png` });
-    await expect(productPage.addedToCartNotification).toBeVisible;
+    //await page.screenshot({ path: addedToCart.png });
+    expect(productPage.addedToCartNotification).toBeVisible;
 });
 
 test.afterAll(async () => {
-    await page.close();
-    await context.tracing.stop({ path: 'newTrace.zip' });
-  });
+    await context.close();
+})
