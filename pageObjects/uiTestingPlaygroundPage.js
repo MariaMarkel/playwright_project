f776658a9c1f92a5bb8c1dc6c7bdb02f254f3b01
MariaMarkel <volkova0512@gmail.com>
2022-04-13 12:30:26 -0400

    update uiplayground.spec.js, data.json

---
+++ pageObjects/uiTestingPlaygroundPage.js
@@ -0,0 +1,89 @@
+const { expect } = require('@playwright/test');
+
+exports.UItestingPlaygroundPage = class UItestingPlaygroundPage {
+
+    constructor (page) {
+        this.page = page;
+        this.btnWithDynamicId = page.locator('.btn.btn-primary');
+        this.primaryBlueBtn = page.locator('.btn.btn-primary.btn-test');
+        this.greenBtnHiddenLayers = page.locator('#greenButton');
+        this.blueBtnHiddenLayers = page.locator('#blueButton');
+        this.loadDelayLink = page.locator('a[href$="/loaddelay"]');
+        this.buttonAppearingAfterDelay = page.locator('.btn.btn-primary');
+        this.buttonTriggeringClientSideLogic = page.locator('.btn.btn-primary');
+        this.DataCalculatedOnClientSide = page.locator('.bg-success');
+        this.columnNamesArray = page.locator('[role="columnheader"]'); //to be able to find index of CPU column
+        this.browserNamesArray = page.locator('[role="row"]>span:nth-child(1)'); //to be able to find the index of Chrome
+        this.btnIgnoresDOMClickEvent = page.locator('.btn.btn-primary');
+        this.btnIgnoresDOMClickEventClicked = page.locator('.btn.btn-success');
+        this.progressBarStartBtn = page.locator('#startButton');
+        this.progressBarStopBtn = page.locator('#stopButton');
+        this.progressBar = page.locator('#progressBar');
+        this.progressBarResultMessage = page.locator('#result');
+        this.hidingBtn = page.locator('#hidingButton');
+        this.nameInputField = page.locator('#name');
+        
+    }
+
+    async gotoHome() {
+        await this.page.goto('http://uitestingplayground.com/');
+    }
+    async gotoDynamicId() {
+        await this.page.goto('http://uitestingplayground.com/dynamicid');
+    }
+    async gotoClassAttr() {
+        await this.page.goto('http://uitestingplayground.com/classattr');
+    }
+    async gotoHiddenLayers() {
+        await this.page.goto('http://uitestingplayground.com/hiddenlayers');
+    }
+    async gotoLoadDelay() {
+        await this.page.goto('http://uitestingplayground.com/loaddelay');
+    }
+    async gotoClientDelay() {
+        await this.page.goto('http://uitestingplayground.com/clientdelay');
+    }
+    async gotoDynamicTable() {
+        await this.page.goto('http://uitestingplayground.com/dynamictable');
+    }
+    async gotoClick() {
+        await this.page.goto('http://uitestingplayground.com/click');
+    }
+    async gotoProgressBar() {
+        await this.page.goto('http://uitestingplayground.com/progressbar');
+    }
+    async gotoScrollbars() {
+        await this.page.goto('http://uitestingplayground.com/scrollbars');
+    }
+    async gotoOverlappedElement() {
+        await this.page.goto('http://uitestingplayground.com/overlapped');
+    }
+
+    async clickBtnWithDynamicId() {
+        await this.btnWithDynamicId.click();
+    }
+    async clickPrimaryBlueBtn() {
+        await this.primaryBlueBtn.click();
+    }
+    async clickBtnHiddenLayers() {
+        await this.greenBtnHiddenLayers.click();
+        await this.blueBtnHiddenLayers.click();
+    }
+    async clickLoadDelayLink() {
+        await this.loadDelayLink.click();
+    }
+    async clickButtonAppearingAfterDelay() {
+        await this.buttonAppearingAfterDelay.click();
+    }
+    async clickButtonTriggeringClientSide() {
+        await this.buttonTriggeringClientSideLogic.click();
+    }
+    async scrollAndClickHidingBtn() {
+        await this.hidingBtn.scrollIntoViewIfNeeded();
+        await this.hidingBtn.click();
+    }
+    async scrollAndFillUpName(name) {
+        await this.nameInputField.scrollIntoViewIfNeeded(2000);
+        await this.nameInputField.fill(name);
+    }
+}
--- tests/uiplayground.spec.js
+++ tests/uiplayground.spec.js
@@ -1,38 +1,130 @@
 const { test, expect } = require('@playwright/test');
-const { DynamicIdPage } = require('../pageObjects/dynamicIdPage');
-
-// test.beforeEach(async ({ page }) => {
-//     const dynamicIdPage = new DynamicIdPage(page); // not sure we need this hook for these tests
-// });
+const { UItestingPlaygroundPage } = require('../pageObjects/uiTestingPlaygroundPage');
+const data = require('../utils/data.json');
 
 test('test 1: dynamic ID', async ({page})  => {
-    const dynamicIdPage = new DynamicIdPage(page);
-    await dynamicIdPage.gotoDynamicId();
-    await dynamicIdPage.clickBtnWithDynamicId();
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoDynamicId();
+    await uiTestingPlaygroundPage.clickBtnWithDynamicId();
     //await page.screenshot({ path: 'test-results/clickBtnWithDynamicId.png' });
 });
 
 test('test 2: class attribute', async ({page})  => {
-    const dynamicIdPage = new DynamicIdPage(page);
-    await dynamicIdPage.gotoClassAttr();
-    await dynamicIdPage.clickPrimaryBlueBtn();
-    await page.screenshot({ path: 'test-results/clickPrimaryBlueBtn.png' });
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoClassAttr();
+    page.on('dialog', dialog => dialog.dismiss());
+    await uiTestingPlaygroundPage.clickPrimaryBlueBtn();
+    //await page.screenshot({ path: 'test-results/clickPrimaryBlueBtn2.png' });
 });
 
-test.skip('test 3', async ({page})  => {
-    const dynamicIdPage = new DynamicIdPage(page);
-    await dynamicIdPage.clickBtnWithDynamicId();
-    await page.screenshot({ path: 'test-results/clickBtnWithDynamicId.png' });
+test('test 3: hidden layers', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoHiddenLayers();
+    await uiTestingPlaygroundPage.clickBtnHiddenLayers();
+    //expect(uiTestingPlaygroundPage.greenBtnHiddenLayers).not.toBeVisible;
+    //expect(uiTestingPlaygroundPage.blueBtnHiddenLayers).not.toBeVisible; //what assertion do i need? everything passes
+    //await page.screenshot({ path: 'test-results/clickBtnHiddenLayers.png' });
 });
 
-test.skip('test 4', async ({page})  => {
-    const dynamicIdPage = new DynamicIdPage(page);
-    await dynamicIdPage.clickBtnWithDynamicId();
-    await page.screenshot({ path: 'test-results/clickBtnWithDynamicId.png' });
+test.fixme('test 4: load delay', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoHome();
+    await uiTestingPlaygroundPage.clickLoadDelayLink(); 
+    //await page.waitForLoadState();
+    //await uiTestingPlaygroundPage.buttonAppearingAfterDelay.waitFor();
+    await uiTestingPlaygroundPage.clickButtonAppearingAfterDelay(); // intentionally remove awaits and add waitFor()
+    //await page.screenshot({ path: 'test-results/loadDelay.png' });
 });
 
-test.skip('test 5', async ({page})  => {
-    const dynamicIdPage = new DynamicIdPage(page);
-    await dynamicIdPage.clickBtnWithDynamicId();
-    await page.screenshot({ path: 'test-results/clickBtnWithDynamicId.png' });
+test.fixme('test 5: client delay', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoClientDelay();
+    await uiTestingPlaygroundPage.clickButtonTriggeringClientSide();
+    await page.waitForSelector('.bg-success'); // i had to hardcode it
+    expect(await uiTestingPlaygroundPage.DataCalculatedOnClientSide).toBeVisible();
+    //await page.screenshot({ path: 'test-results/clientDelay.png' });
+});
+
+test.only('test 6: dynamic table', async ({page})  => {
+    // cy.visit('http://uitestingplayground.com/dynamictable');
+    // const homePage = new HomePage();
+    // let cpuColumnIndex;
+    // let cpuPercentArray = [];
+    // let chromeRowIndex;
+    // let allCPUs;
+    // let chromeCPU;
+    // homePage.browserNamesArray().each((el, i) => {
+    //     if (el.text() === 'Chrome') {
+    //         chromeRowIndex = i;
+    //         console.log("*****************chromeRowIndex "+ chromeRowIndex);
+    //     }
+    // });
+    // homePage.columnNamesArray().each( (el, i) => {
+    //     if (el.text() === 'CPU') {
+    //         cpuColumnIndex = i;
+    //         console.log("*****************cpuColumnIndex "+ cpuColumnIndex);
+    //         allCPUs = cy.get(`[role="row"]>span:nth-child(${cpuColumnIndex+1})`); //CPU column elements = array
+    //         allCPUs.each((el, i) => {
+    //             //console.log(el.text());
+    //             cpuPercentArray.push(el.text());
+    //         })
+    //             .then(function () {
+    //                 chromeCPU = cpuPercentArray[chromeRowIndex];
+    //                 console.log(chromeCPU);
+    //                 homePage.chromeCPUmessage().should('contain', chromeCPU)
+    //             });
+    //     }
+    // });
+
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoDynamicTable();
+    let cpuColumnIndex;
+    let cpuPercentArray = [];
+    let chromeRowIndex;
+    let allCPUs;
+    let chromeCPU;
+    let browserNamesArray = await page.$$('[role="row"]>span:nth-child(1)');
+    browserNamesArray.forEach((el, i) => {        //uiTestingPlaygroundPage.browserNamesArray
+            // if (el.innerText() === 'Chrome') {
+            //     chromeRowIndex = i;
+            //     console.log("*****************chromeRowIndex "+ chromeRowIndex);
+            // }
+            //console.log(el); //(await this._elementChannel.innerText()).value;
+
+            console.log(el.innerText);
+            });
+});
+
+test('test 7: click', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoClick();
+    await uiTestingPlaygroundPage.btnIgnoresDOMClickEvent.click()
+    expect(await uiTestingPlaygroundPage.btnIgnoresDOMClickEventClicked).toBeVisible();
+    await page.waitForTimeout(2000); //for screenshot
+    //await page.screenshot({ path: 'test-results/btnClicked.png' });
+});
+
+test.fixme('test 8: progress bar', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoProgressBar();
+    const progressPercent = parseInt(await uiTestingPlaygroundPage.progressBar.innerText()); // 25 initially
+    console.log("***********" + progressPercent)
+    await uiTestingPlaygroundPage.progressBarStartBtn.click();
+    //await page.waitForTimeout(5000);
+    await uiTestingPlaygroundPage.progressBarStopBtn.click();
+    await page.screenshot({ path: 'test-results/progress1.png' });
+});
+
+test('test 9: scrollbars', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoScrollbars();
+    await uiTestingPlaygroundPage.scrollAndClickHidingBtn();
+    //await page.screenshot({ path: 'test-results/scrollbar1.png' });
+});
+
+test('test 10: overlapped element', async ({page})  => {
+    const uiTestingPlaygroundPage = new UItestingPlaygroundPage(page);
+    await uiTestingPlaygroundPage.gotoOverlappedElement();
+    await uiTestingPlaygroundPage.scrollAndFillUpName(data.name);
+    //await page.screenshot({ path: 'test-results/overlapped.png' });
 });
